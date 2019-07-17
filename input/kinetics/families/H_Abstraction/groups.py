66c64c15a7a13c710057f6ce40371c4060b92283
alongd <alongd@mit.edu>
2019-07-17 16:17:07 -0400

    Don't H abstract from a radical site

--- input/kinetics/families/H_Abstraction/groups.py
+++ input/kinetics/families/H_Abstraction/groups.py
@@ -7905,3 +7905,17 @@ u"""
 
 """,
 )
+
+forbidden(
+    label = "radical",
+    group =
+"""
+1 *1 R!H u1 px cx {2,S}
+2 *2 H   u0 p0 c0 {1,S}
+""",
+    shortDesc = u"""""",
+    longDesc =
+u"""
+
+""",
+)
